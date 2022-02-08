3520c26c11801ee3db98bbd6a61ddb19b32f58f1
Max Yermakhanov <max@automagically.io>
2022-02-08 11:13:23 -0500

    draft

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     return Settings.sync(context.octokit, repo, config)
   }
 
-  robot.on('push', async context => {
-    const { payload } = context
-    const { repository } = payload
+  // robot.on('push', async context => {
+  //   const { payload } = context
+  //   const { repository } = payload
 
-    const defaultBranch = payload.ref === 'refs/heads/' + repository.default_branch
-    if (!defaultBranch) {
-      robot.log.debug('Not working on the default branch, returning...')
-      return
-    }
+  //   const defaultBranch = payload.ref === 'refs/heads/' + repository.default_branch
+  //   if (!defaultBranch) {
+  //     robot.log.debug('Not working on the default branch, returning...')
+  //     return
+  //   }
 
-    const settingsModified = payload.commits.find(commit => {
-      return commit.added.includes(Settings.FILE_NAME) ||
-        commit.modified.includes(Settings.FILE_NAME)
-    })
+  //   const settingsModified = payload.commits.find(commit => {
+  //     return commit.added.includes(Settings.FILE_NAME) ||
+  //       commit.modified.includes(Settings.FILE_NAME)
+  //   })
 
-    if (!settingsModified) {
-      robot.log.debug(`No changes in '${Settings.FILE_NAME}' detected, returning...`)
-      return
-    }
+  //   if (!settingsModified) {
+  //     robot.log.debug(`No changes in '${Settings.FILE_NAME}' detected, returning...`)
+  //     return
+  //   }
 
-    return syncSettings(context)
-  })
+  //   return syncSettings(context)
+  // })
 
-  robot.on('repository.edited', async context => {
-    const { payload } = context
-    const { changes, repository } = payload
+  // robot.on('repository.edited', async context => {
+  //   const { payload } = context
+  //   const { changes, repository } = payload
 
-    if (!Object.prototype.hasOwnProperty.call(changes, 'default_branch')) {
-      robot.log.debug('Repository configuration was edited but the default branch was not affected, returning...')
-      return
-    }
+  //   if (!Object.prototype.hasOwnProperty.call(changes, 'default_branch')) {
+  //     robot.log.debug('Repository configuration was edited but the default branch was not affected, returning...')
+  //     return
+  //   }
 
-    robot.log.debug(`Default branch changed from '${changes.default_branch.from}' to '${repository.default_branch}'`)
+  //   robot.log.debug(`Default branch changed from '${changes.default_branch.from}' to '${repository.default_branch}'`)
 
-    return syncSettings(context)
-  })
+  //   return syncSettings(context)
+  // })
 
   robot.on('repository.created', async context => {
     return syncSettings(context)
